4afec32d02b3f1c99c565fcd706f266d0de67e15
chunderella <sijy19@naver.com>
2022-11-16 01:19:48 +0900

    a

--- app/src/main/java/com/goteatfproject/appgot/web/HomeController.java
+++ app/src/main/java/com/goteatfproject/appgot/web/HomeController.java
@@ -1,13 +1,39 @@
 package com.goteatfproject.appgot.web;
 
+import com.goteatfproject.appgot.service.EventService;
+import com.goteatfproject.appgot.service.FeedService;
+import com.goteatfproject.appgot.service.MemberService;
+import com.goteatfproject.appgot.service.PartyService;
+import com.goteatfproject.appgot.vo.Event;
+import com.goteatfproject.appgot.vo.Feed;
+import com.goteatfproject.appgot.vo.Party;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.ServletContext;
 
 @Controller
 public class HomeController {
 
+  PartyService partyService;
+  FeedService feedService;
+EventService eventService;
+  ServletContext sc;
+
+  public HomeController(PartyService partyService, FeedService feedService,
+                        EventService eventService, ServletContext sc) {
+    this.partyService = partyService;
+    this.feedService = feedService;
+    this.eventService = eventService;
+    this.sc = sc;
+  }
+
   @GetMapping("/")
-  public String home() {
+  public String List(Model model) throws Exception {
+    model.addAttribute("parites",partyService.list());
+    model.addAttribute("events",eventService.list());
+    model.addAttribute("feeds",feedService.list());
     return "index";
   }
 }
--- app/src/main/java/com/goteatfproject/appgot/web/filter/AdminCheckFilter.java
+++ app/src/main/java/com/goteatfproject/appgot/web/filter/AdminCheckFilter.java
@@ -14,29 +14,29 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AdminCheckFilter implements Filter {
-  
+
   @Override
   public void init(FilterConfig filterConfig) throws ServletException {
     System.out.println("AdminCheckFilter.init() 실행!");
   }
-  
+
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-    
+          throws IOException, ServletException {
+
     HttpServletRequest httpRequest = (HttpServletRequest) request;
     HttpServletResponse httpResponse = (HttpServletResponse) response;
-    
+
     System.out.println("AdminCheckFilter.doFilter() 실행!");
     if (httpRequest.getServletPath().startsWith("/admin")) {
       Member loginMember = (Member) httpRequest.getSession().getAttribute("loginMember");
       if (loginMember == null || // 로그인이 안됐거나
-          !loginMember.getId().equals("admin@test.com")) { // 관리자가 아니라면
+              !loginMember.getId().equals("admin@test.com")) { // 관리자가 아니라면
         httpResponse.sendRedirect(httpRequest.getContextPath() + "/auth/login");
         return;
       }
     }
-    
+
     chain.doFilter(request, response);
   }
 
